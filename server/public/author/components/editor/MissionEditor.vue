7ba4c255f759d2b89e1f0435c8a5cbc235e216c4
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-05-08 19:34:11 +0200

    Added empty mission controller editor

--- server/public/author/components/editor/MissionEditor.vue
+++ server/public/author/components/editor/MissionEditor.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="mission-form">
-        <mission-form :mission-head="missionHead"></mission-form>
+        <b-row>
+            <b-col>
+                <mission-form :mission-head="missionHead"></mission-form>
+            </b-col>
+            <b-col>
+                <mission-content-editor></mission-content-editor>
+            </b-col>
+        </b-row>
     </div>
 </template>
 
@@ -16,7 +23,8 @@
         methods: {
         },
         components: {
-            'mission-form': httpVueLoader('/author/components/editor/MissionHeadForm.vue')
+            'mission-form': httpVueLoader('/author/components/editor/MissionHeadForm.vue'),
+            'mission-content-editor': httpVueLoader('/author/components/editor/content-editor/MissionContentEditor.vue')
         }
     }
 </script>
