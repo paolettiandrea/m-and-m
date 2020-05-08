14e0c21e0b782a78c27f10730a2fdc7ea42efe91
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-05-08 19:09:40 +0200

    Some routing (mostly temporary) and schema management (also temp)
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/routes/missions.js
+++ server/routes/missions.js
@@ -2,7 +2,15 @@ const express = require('express')
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.json([{title: "Yoyoyo"}, { title: "Yayayay"}])
+    res.json([
+        {
+            title: "A title",
+            summary: "A short little description"
+        },
+        {
+            title: "Another title",
+            summary: "Another short little descritpion"
+        }])
 })
 
 module.exports = router
