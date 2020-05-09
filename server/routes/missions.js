165ff6310f931e4c3886bed4b0dd9ea0b75f1972
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-05-09 15:45:30 +0200

    Updated data
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/routes/missions.js
+++ server/routes/missions.js
@@ -1,18 +1,12 @@
+const fs = require('fs');
 const express = require('express')
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-    res.json([                  //TEMP
-        {
-            uid: 1,
-            title: "A title",
-            summary: "A short little description"
-        },
-        {
-            uid: 2,
-            title: "Another title",
-            summary: "Another short little descritpion"
-        }])
+    fs.readFile("data/missions.json", 'utf-8', ((err, data) => {
+        console.log();
+        res.json(JSON.parse(data).availableMissions)
+    }));
 })
 
 module.exports = router
