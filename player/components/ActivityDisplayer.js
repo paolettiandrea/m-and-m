4530af68d2144e86c4ba4b8d7bff9704268181be
stefi98 <stefania.rizzo6@studio.unibo.it>
2020-05-31 13:31:32 +0200

    text-insert

--- player/components/ActivityDisplayer.js
+++ player/components/ActivityDisplayer.js
@@ -1,7 +1,7 @@
 Vue.component("mission-displayer", {
     template: `
-        <div> 
-        <transition name="content-slide" mode="out-in">  
+        <div>
+        <transition name="content-slide" mode="out-in">
                 <div class="activity-displayer-div" v-if="this.pointedActivity" :key="this.pointedActivity.uid">
                     <activity-displayer :activityContent="this.pointedActivity"></activity-displayer>
                 </div>
@@ -53,7 +53,7 @@ Vue.component("activity-displayer", {
                     <div v-if="this.activityContent">
                         <div  v-for="contentChunk of this.activityContent.content">
                             <component :is="contentChunk.type" :data="contentChunk.data"></component>
-                        </div>    
+                        </div>
                     </div>
                 <!-- </transiton> -->
                 </div>`,
@@ -125,19 +125,18 @@ Vue.component("img-displayer", {
 Vue.component("chat", {
     template: `<div>
         <button class="open-button" v-on:click="openForm">Chat</button>
-
     <div class="chat-popup" id="myForm">
       <form action="/action_page.php" class="form-container">
         <h1>Chat</h1>
-    
+
         <label for="msg"><b>Message</b></label>
         <textarea placeholder="Type message.." name="msg" required></textarea>
-    
+
         <button type="submit" class="btn">Send</button>
         <button type="button" class="btn cancel" v-on:click="closeForm">Close</button>
       </form>
     </div>
-    
+
 </div>`
     ,
 
@@ -155,9 +154,98 @@ Vue.component("chat", {
 })
 
 
+Vue.component("multiple-checkboxes", {
+    template: `<div>
+        <template>
+  <div>
 
+    <b-form-group label="Multiple checkboxes form">
+      <b-form-checkbox-group
+        v-model="selected"
+        :options="options"
+        buttons
+        button-variant="primary"
+        size="lg"
+        name="buttons-2"
+      ></b-form-checkbox-group>
+    </b-form-group>
+    <div>Selected: <strong>{{ selected }}</strong></div>
+  </div>
+</template>
+    </div>`,
 
+  data() {
+    return {
+      selected: [],
+      options: [
+        { text: 'Orange', value: 'orange' },
+        { text: 'Apple', value: 'apple' },
+        { text: 'Pineapple', value: 'pineapple' },
+        { text: 'Grape', value: 'grape' }
+      ]
+    }
+  }
+})
 
+Vue.component("text-insert",{
+  template: `<div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+      <b-form-group
+        id="input-group-1"
+        label="Inserisci la tua risposta:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.text"
+          type="text"
+          required
+          placeholder="Inserisci la tua risposta"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+    <b-card class="mt-3" header="Form Data Result">
+      <pre class="m-0">{{ form }}</pre>
+    </b-card>
+  </div> `,
+
+  data() {
+     return {
+       form: {
+         text: ''
+       },
+       show: true
+     }
+   },
+   methods: {
+     openForm(text) {
+         document.getElementById("input-1").style.display = "block";
+         if(this.form.text=="Risposta")
+          {
+            this.data.text="Risposta esatta";
+            return this.data.text;
+          }
+
+     },
+     onSubmit(evt) {
+       evt.preventDefault()
+       alert(JSON.stringify(this.form))
+     },
+     onReset(evt) {
+       evt.preventDefault()
+       // Reset our form values
+       this.form.text = ''
+       // Trick to reset/clear native browser form validation state
+       this.show = false
+       this.$nextTick(() => {
+         this.show = true
+       })
+     }
+   }
+})
 
 
 
@@ -168,11 +256,9 @@ Vue.component("chat", {
 
 /*
 <style scoped>
-
     .mission-form {
         overflow-x: hidden;
     }
-
     .content-slide-enter-active, .content-slide-leave-active {
         transition: all 0.5s;
     }
@@ -186,10 +272,8 @@ Vue.component("chat", {
     opacity: 0;
     transform: translateX(100 % );
 }
-
 .labeled - form - group {
     label - cols: 3
 }
-
 <
 /style>*/
