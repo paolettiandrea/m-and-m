e61e58863e459f71d253b945312eb1483805dc72
om4r-42097 <omar97rida@gmail.com>
2020-06-14 20:26:05 +0200

    draw-canvas&click-img&video-displayer

--- player/components/ActivityDisplayer.js
+++ player/components/ActivityDisplayer.js
@@ -1,12 +1,12 @@
 Vue.component("mission-displayer", {
     template: `
-        <div>
+        <div align="center">
         <transition name="content-slide" mode="out-in">
                 <div class="activity-displayer-div" v-if="this.pointedActivity" :key="this.pointedActivity.uid">
                     <activity-displayer :activityContent="this.pointedActivity"></activity-displayer>
                 </div>
         </transition>
-        <button v-on:click="nextActivity">Next mission</button>
+        <button  v-on:click="nextActivity">Next mission</button>
         <chat></chat>
         </div>
     `,
@@ -119,6 +119,7 @@ Vue.component("card-displayer", {
         data: null
     },
     data() {
+        align = "center"
         return {
             test: "M&M.jpeg"
         }
@@ -146,8 +147,7 @@ Vue.component("chat", {
         <button type="button" class="btn cancel" v-on:click="closeForm">Close</button>
       </form>
     </div>
-</div>`
-    ,
+</div>`,
 
     methods: {
         openForm() {
@@ -196,8 +196,8 @@ Vue.component("multiple-checkboxes", {
     }
 })
 
-Vue.component("text-insert",{
-  template: `<div>
+Vue.component("text-insert", {
+    template: `<div>
   <div role="group" >
   <label for="input-live">Inserisci la tua risposta:</label>
   <b-form-input
@@ -221,23 +221,23 @@ Vue.component("text-insert",{
 </div>
   </div> `,
 
-   props: {
-      data: null
-   },
-   computed: {
-      textState() {
-        return this.Answer==this.data.correctAnswer ? true : false
-      },
-      dynamicStyle(){
-        return {
-          color: ``
+    props: {
+        data: null
+    },
+    computed: {
+        textState() {
+            return this.Answer == this.data.correctAnswer ? true : false
+        },
+        dynamicStyle() {
+            return {
+                color: ``
+            }
         }
-      }
     },
     data() {
-      return {
-        Answer: ''
-      }
+        return {
+            Answer: ''
+        }
     }
 
 
@@ -250,34 +250,12 @@ Vue.component("text-insert",{
 
 
 
-/*
-<style scoped>
-    .mission-form {
-        overflow-x: hidden;
-    }
-    .content-slide-enter-active, .content-slide-leave-active {
-        transition: all 0.5s;
-    }
-    .content-slide-enter, .content-slide-leave-to /* .list-leave-active below version 2.1.8 */
-/*
-{
-    opacity: 0;
-    transform: translateX(-100 % );
-}
-.content - slide - leave - to {
-    opacity: 0;
-    transform: translateX(100 % );
-}
-.labeled - form - group {
-    label - cols: 3
-}
-<
-/style>*/
+
 Vue.component("canvas-displayer", {
     template: `
 
-    <div>
-        <div id="canvas" v-on:mousemove="coordinate" v-on:click="viewCoordianate">{{x}},{{y}}</div>
+    <div  align="center">
+        <div id="canva" v-on:mousemove="coordinate" v-on:click="viewCoordianate"><p>{{x}},{{y}}</p></div>
 
     </div>
     `,
@@ -297,3 +275,183 @@ Vue.component("canvas-displayer", {
         }
     }
 })
+
+
+
+Vue.component("click-img", {
+    template: `
+    <div :align="data.pos" >
+        <img :src="data.url" alt="Workplace" usemap="#workmap" :width="data.w" :height="data.h" v-on:click="test">
+
+        <map name="workmap">
+        <area shape="rect" :coords="data.computer" alt="Computer" v-on:click="imgClick('Computer')">
+        <area shape="rect" :coords="data.telefono" alt="Phone" v-on:click="imgClick('iPhone')">
+        <area shape="circle" :coords="data.caffe" alt="Cup of coffee" v-on:click="imgClick('Caffè')">
+        </map>
+    </div>
+    `,
+    props: {
+        data: null
+    },
+    methods: {
+        imgClick(x) {
+            alert(x);
+        },
+        test() {
+            alert("NO");
+        }
+    }
+
+
+})
+
+
+Vue.component("video-displayer", {
+    template: `
+    <div  :align="data.pos">
+        <video :width="data.w"  controls>
+            <source :src="data.url" type="video/mp4">
+        </video>
+    </div>
+    `,
+    props: {
+        data: null
+    }
+
+})
+
+
+Vue.component("canvas-draw", {
+    template: `
+
+    <div  align="center" id="drawing">
+    <h1>Canvas Demo</h1>
+    <canvas ref="canvas" id='drawing-pad' :width="data.w" :height="data.h">
+      This is an interactive drawing pad.
+    </canvas>
+    <div>
+      <button @click='resetCanvas'>Reset Canvas</button>
+      <button @click='saveImage'>Save Image</button>
+      <button @click='replay'>Replay</button>
+    </div>
+    <hr>
+    <img ref="img" src="" id='canvas-image'>
+    <small>Right click to save image.</small>
+  </div>
+    `,
+    props: {
+        data: null
+    },
+    data() {
+        return {
+            canvas: null,
+            context: null,
+            isDrawing: false,
+            startX: 0,
+            startY: 0,
+            points: [],
+        }
+    },
+    mounted() {
+        var vm = this
+        vm.canvas = vm.$refs.canvas
+        vm.context = vm.canvas.getContext("2d");
+        vm.canvas.addEventListener('mousedown', vm.mousedown);
+        vm.canvas.addEventListener('mousemove', vm.mousemove)
+        document.addEventListener('mouseup', vm.mouseup);
+    },
+    methods: {
+        mousedown(e) {
+            var vm = this
+            var rect = vm.canvas.getBoundingClientRect();
+            var x = e.clientX - rect.left;
+            var y = e.clientY - rect.top;
+
+            vm.isDrawing = true;
+            vm.startX = x;
+            vm.startY = y;
+            vm.points.push({
+                x: x,
+                y: y
+            });
+        },
+        mousemove(e) {
+            var vm = this
+            var rect = vm.canvas.getBoundingClientRect();
+            var x = e.clientX - rect.left;
+            var y = e.clientY - rect.top;
+
+            if (vm.isDrawing) {
+                vm.context.beginPath();
+                vm.context.moveTo(vm.startX, vm.startY);
+                vm.context.lineTo(x, y);
+                vm.context.lineWidth = 1;
+                vm.context.lineCap = 'round';
+                vm.context.strokeStyle = "rgba(255, 0, 0, 1)";
+                vm.context.stroke();
+
+                vm.startX = x;
+                vm.startY = y;
+
+                vm.points.push({
+                    x: x,
+                    y: y
+                });
+            }
+        },
+        mouseup(e) {
+            var vm = this
+            vm.isDrawing = false;
+            if (vm.points.length > 0) {
+                localStorage['points'] = JSON.stringify(vm.points);
+            }
+        },
+        resetCanvas() {
+            var vm = this
+            vm.canvas.width = vm.canvas.width;
+            vm.points.length = 0; // reset points array
+        },
+        saveImage() {
+            var vm = this
+            var dataURL = vm.canvas.toDataURL();
+            var img = vm.$refs.img;
+            img.src = dataURL;
+        },
+        replay() {
+            var vm = this
+            vm.canvas.width = vm.canvas.width;
+
+            // load localStorage
+            if (vm.points.length === 0) {
+                if (localStorage["points"] !== undefined) {
+                    vm.points = JSON.parse(localStorage["points"]);
+                }
+            }
+
+            var point = 1;
+            setInterval(function() {
+                drawNextPoint(point);
+                point += 1;
+            }, 10);
+
+            function drawNextPoint(index) {
+                if (index >= vm.points.length) {
+                    return;
+                }
+                var startX = vm.points[index - 1].x;
+                var startY = vm.points[index - 1].y;
+
+                var x = vm.points[index].x;
+                var y = vm.points[index].y;
+
+                vm.context.beginPath();
+                vm.context.moveTo(startX, startY);
+                vm.context.lineTo(x, y);
+                vm.context.lineWidth = 1;
+                vm.context.lineCap = 'round';
+                vm.context.strokeStyle = "rgba(255, 0, 0, 1)";
+                vm.context.stroke();
+            }
+        },
+    }
+}).$mount('#drawing')
