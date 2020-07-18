1098f6dcd88be45a1df86176f4dae954d2f9a133
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-18 18:57:36 +0200

    Fixed relative paths
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/index.js
+++ server/index.js
@@ -1,16 +1,52 @@
-var express = require('express');
-var app = express();
-var path = require('path')
+// importing the dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const morgan = require('morgan');
+const path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+const app = express();
+const server = require('http').Server(app);
 
+// const io = require('socket.io')(server);
+// io.on('connection', (socket) => {
+//   socket.on('disconnect', () => {
+//     console.log("A user disconnected");
+//   });
+// });
+
+// TEMP stuff
+const a = require(path.join(__dirname, '/routes/missions'));
+const api = require(path.join(__dirname, './routes/api'))
+
+
+// MIDDLEWARE
+app.use(bodyParser.json());                  // using bodyParser to parse JSON bodies into js objects
+app.use(cors());                             // enabling CORS for all requests
+app.use(morgan('combined'));          // adding morgan to log HTTP requests
+app.use(fileUpload({debug: true}));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'data/resources')))
+
+app.use('/player', express.static(path.join(__dirname, '../player')));
 app.use('/author', express.static(path.join(__dirname, '../author')));
+app.use('/supervisor', express.static(path.join(__dirname, '../supervisor')));
+app.use('/common', express.static(path.join(__dirname, '../common')));
+
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+// ROUTING
+app.get('/', function (req, res) {
+  res.send("Hello, I'm the server");
+})
 
 
+app.use("/missions", a)
+app.use("/api", api)
 
-app.listen(8000, function () {
-  console.log('Example app listening on port 8000!');
+// STARTING THE SERVER
+server.listen(8000, () => {
+  console.log('listening on port 8000');
 });
-
