635a2dfe4aa0fa3118659f8dba2de2f568cee708
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 16:17:09 +0200

    Tests
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/index.js
+++ server/index.js
@@ -17,25 +17,25 @@ const server = require('http').Server(app);
 // });
 
 // TEMP stuff
-const a = require(path.join(__dirname, '/routes/missions'));
-const api = require(path.join(__dirname, './routes/api'))
+// const a = require(path.join(__dirname, './routes/missions'));
+// const api = require(path.join(__dirname, './routes/api'))
 
 
 // MIDDLEWARE
-app.use(bodyParser.json());                  // using bodyParser to parse JSON bodies into js objects
-app.use(cors());                             // enabling CORS for all requests
-app.use(morgan('combined'));          // adding morgan to log HTTP requests
-app.use(fileUpload({debug: true}));
+// app.use(bodyParser.json());                  // using bodyParser to parse JSON bodies into js objects
+// app.use(cors());                             // enabling CORS for all requests
+// app.use(morgan('combined'));          // adding morgan to log HTTP requests
+// app.use(fileUpload({debug: true}));
+//
+// app.use(express.static(path.join(__dirname, './public')));
+// app.use(express.static(path.join(__dirname, './data/resources')))
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'data/resources')))
-
-app.use('/player', express.static(path.join(__dirname, '../player')));
+// app.use('/player', express.static(path.join(__dirname, '../player')));
 app.use('/author', express.static(path.join(__dirname, '../author')));
-app.use('/supervisor', express.static(path.join(__dirname, '../supervisor')));
-app.use('/common', express.static(path.join(__dirname, '../common')));
-
-app.use(express.static(path.join(__dirname, 'node_modules')));
+// app.use('/supervisor', express.static(path.join(__dirname, '../supervisor')));
+// app.use('/common', express.static(path.join(__dirname, '../common')));
+//
+// app.use(express.static(path.join(__dirname, 'node_modules')));
 
 // ROUTING
 app.get('/', function (req, res) {
