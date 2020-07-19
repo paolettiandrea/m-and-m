47fed50b9fcd2334b7bf5bbe9c110bec75e3ca8e
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 17:16:28 +0200

    Tests
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/js/altDbController.js
+++ server/js/altDbController.js
@@ -20,7 +20,10 @@ function missionContentFile(id) { return path.join(missionDirectory(id), mission
 // Returns an object containing all the mission heads keyed by their unique ids
 async function getMissionHeadsList() {
     return new Promise((resolve) => {
-        resolve({test: 'succeded'})
+        fs.readFile(activeMissionsHeadListPath,  'utf-8', (err, data) => {
+            if (err) resolve(err);
+            resolve(data);
+        })
     });
 }
 
