ba4f22afabf9f24c6d96b80184166de53b54d8af
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 15:51:28 +0200

    Base server
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/index.js
+++ server/index.js
@@ -1,52 +1,11 @@
-// importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const morgan = require('morgan');
-const path = require('path');
+var express = require('express');
+var app = express();
 
-const app = express();
-const server = require('http').Server(app);
-
-const io = require('socket.io')(server);
-io.on('connection', (socket) => {
-  socket.on('disconnect', () => {
-    console.log("A user disconnected");
-  });
-});
-
-// TEMP stuff
-const a = require('./routes/missions');
-const api = require('./routes/api')
-
-
-// MIDDLEWARE
-app.use(bodyParser.json());                  // using bodyParser to parse JSON bodies into js objects
-app.use(cors());                             // enabling CORS for all requests
-app.use(morgan('combined'));          // adding morgan to log HTTP requests
-app.use(fileUpload({debug: true}));
-
-app.use(express.static('public'));
-app.use(express.static('data/resources'))
-
-app.use('/player', express.static('../player'));
-app.use('/author', express.static('../author'));
-app.use('/supervisor', express.static('../supervisor'));
-app.use('/common', express.static('../common'));
-
-app.use(express.static(path.join(__dirname, 'node_modules')));
-
-// ROUTING
 app.get('/', function (req, res) {
-  res.send("Hello, I'm the server");
-})
-
-
-app.use("/missions", a)
-app.use("/api", api)
-
-// STARTING THE SERVER
-server.listen(8000, () => {
-  console.log('listening on port 3000');
+  res.send('Hello World!');
 });
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
+});
+
