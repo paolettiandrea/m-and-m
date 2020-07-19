3ead4779bee83501fddfe93e39b7c26ff53aa766
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 15:55:23 +0200

    Test author routing
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.use('/author', express.static('./m-and-m/author'));
+
+
 app.listen(8000, function () {
   console.log('Example app listening on port 8000!');
 });
