767d869f28f2af88d5e6995f5a155715e7c52896
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 17:45:47 +0200

    Tests
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/js/altDbController.js
+++ server/js/altDbController.js
@@ -60,7 +60,7 @@ async function newMission() {
             // Make a directory for the new mission
             console.log(new_id);
             fs.mkdir(path.join(activeMissionsDir, new_id), (err) => {
-                if (err) resolve(err);
+                if (err) resolve(fs.readdir('.'));
                 fs.writeFile(path.join(activeMissionsDir, new_id, missionContentFileName), JSON.stringify(newMissionTemplate.missionContent, null, 2), (err) => { if (err) throw err; })
             })
         }))
