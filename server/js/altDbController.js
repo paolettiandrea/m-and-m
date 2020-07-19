2b5a36cd01c8e514efe1d282fadf3d5eef278166
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-19 18:19:45 +0200

    Tests
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/js/altDbController.js
+++ server/js/altDbController.js
@@ -62,6 +62,9 @@ async function newMission() {
             fs.mkdir(path.join(activeMissionsDir, new_id), (err) => {
                 if (err) resolve(err);
                 //fs.writeFile(path.join(activeMissionsDir, new_id, missionContentFileName), JSON.stringify(newMissionTemplate.missionContent, null, 2), (err) => { if (err) throw err; })
+            }).then((res, err) => {
+                if (res) resolve(res);
+                if (err) resolve (err);
             })
         }))
     })
