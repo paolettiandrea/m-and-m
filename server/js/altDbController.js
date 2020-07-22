d2fe15c8d57f7be6dc5ea14c8c0dd3e458b76d63
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-22 17:05:27 +0200

    test
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/js/altDbController.js
+++ server/js/altDbController.js
@@ -53,17 +53,17 @@ async function newMission() {
             if (err) {resolve(err);}
             newMissionTemplate = JSON.parse(newMissionTemplate);
             // Add new missionHead to the list
-            // fs.readFile(missionsHeadsPath, 'utf-8', ((err, missionHeads) => {      // TODO when implementing archive missions can be created in it
-            //     if (err) resolve(err);
-            //     missionHeads = JSON.parse(missionHeads);
-            //     missionHeads[new_id] = newMissionTemplate.missionHead;
-            //     fs.writeFile(missionsHeadsPath, JSON.stringify(missionHeads, null, 2), (err) => { if (err){ resolve(err);}})
-            //     // resolve({
-            //     //     id: new_id,
-            //     //     head: missionHeads[new_id],
-            //     //     content: newMissionTemplate.missionContent
-            //     // })
-            // }))
+            fs.readFile(missionsHeadsPath, 'utf-8', ((err, missionHeads) => {      // TODO when implementing archive missions can be created in it
+                if (err) resolve(err);
+                missionHeads = JSON.parse(missionHeads);
+                missionHeads[new_id] = newMissionTemplate.missionHead;
+                fs.writeFile(missionsHeadsPath, JSON.stringify(missionHeads, null, 2), (err) => { if (err){ resolve(err);}})
+                resolve({
+                    id: new_id,
+                    head: missionHeads[new_id],
+                    content: newMissionTemplate.missionContent
+                })
+            }))
             // Make a directory for the new mission
             let missionPath = missionDirectory(new_id);
             fs.mkdir(missionPath, {recursive: true}, (err) => {
