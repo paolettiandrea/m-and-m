63ca1abc08736a1fcf614850fd9d85b0a0908b95
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2020-07-27 15:35:17 +0200

    Supervisor now can see the unsupervised list at startup
    Signed-off-by: Andrea Paoletti <andrea.paoletti3@studio.unibo.it>

--- server/js/socket/SocketManager.js
+++ server/js/socket/SocketManager.js
@@ -33,9 +33,9 @@ function initialize(server) {
 function supervisorConnectedCallback(supSocket) {
     supSocket.join('supervisors');
     activeRooms[supSocket.id] = new SupervisedRoom(supSocket);
-    // for (const unsuPlayer in unsupervisedPlayers) {
-    //     supSocket.emit('player-connected', unsuPlayer.model);
-    // }
+    for (const unsuPlayer in unsupervisedPlayers) {     // To the connected supervisor all the unsupervised players look like they just connected
+        supSocket.emit('player-connected', unsupervisedPlayers[unsuPlayer].model);      // TODO maybe an initialization event would be better on the long term
+    }
     console.log('New supervisor connected');
 }
 
@@ -53,11 +53,25 @@ function playerConnectedCallback(playerSocket) {
     }
     unsupervisedPlayers[playerSocket.id] = newPlayer;
     io.in('supervisors').emit('player-connected', newPlayer.model);
+
+    printStatus();
 }
 
 function playerDisconnectedCallback(playerSocket) {
     console.log('Player disconnected');
+    if (unsupervisedPlayers.hasOwnProperty(playerSocket.id)) {
+        delete unsupervisedPlayers[playerSocket.id];
+    }
     io.in('supervisors').emit('player-disconnected', {socketId: playerSocket.id});
+}
+
+function printStatus() {
+    let s = '';
+    s+= 'Unsupervised players:\n';
+    for (const player in unsupervisedPlayers) {
+        s+= '\t' + player + '\n';
+    }
+    console.log(s);
 }
 
 
