5f28fba1a2fefb21b7078892c1a7d02600f33314
Andrea Paoletti <andrea.paoletti3@studio.unibo.it>
2021-01-29 23:05:23 +0100

    No fix :(

--- player/components/activity-displayer/input-components/CanvasDraw.js
+++ player/components/activity-displayer/input-components/CanvasDraw.js
@@ -144,14 +144,5 @@ Vue.component("canvas-draw", {
         vm.context.stroke();
       }
     },
-  },
-  mounted() {
-      $(document).ready(function(e) {
-            $('img[usemap]').rwdImageMaps();
-            
-            $('area').on('click', function() {
-                //alert($(this).attr('alt') + ' clicked');
-            });
-        });
   }
 })
